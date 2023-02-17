519fc6acc5c24774222730def77250690a402594
carlosyahir <68354811+Tapioka0@users.noreply.github.com>
2023-02-16 19:13:51 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, res) {
-  res.send('hellos');
+  res.send('sosas');
 });
 
 app.listen(process.env.PORT || 3000);
