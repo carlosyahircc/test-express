e01eb565d0a52e719c56f09d45ad13bf7d8281da
carlos_castillo <carlos.castillo@sagaon.tech>
2023-02-17 10:09:58 -0600

    djasjd

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var app = express();
-
+require("dotenv").config();
 app.get("/", function (req, res) {
-  res.send(process.env.token);
+  res.send(process.env.token + "sadsa");
 });
 
 app.listen(process.env.PORT || 3000);
